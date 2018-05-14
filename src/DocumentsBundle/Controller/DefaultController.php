2d4fdfecb677be4469e4e0f0d42b87f1a58b47b1
Tylp <azertyzhd@gmail.com>
2018-05-14 16:55:55 +0200

    route

--- src/DocumentsBundle/Controller/DefaultController.php
+++ src/DocumentsBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('DocumentsBundle:Default:index.html.twig');
+        return $this->render('@Documents/Default/index.html.twig');
     }
 }
